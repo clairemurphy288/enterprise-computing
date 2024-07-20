4126a8d6b9de0492737085a343c1799b7dfba8bf
Claire Murphy <murphyclaire91@gmail.com>
2024-07-20 18:23:30 -0400

    fixed starting before video

--- src/VideoPage.js
+++ src/VideoPage.js
@@ -11,7 +11,7 @@ const VideoPage = () => {
   const [currentText, setCurrentText] = useState('');
   const [currentIndex, setCurrentIndex] = useState(0);
   const [player, setPlayer] = useState(null);
-  const [isPlaying, setIsPlaying] = useState(true);
+  const [isPlaying, setIsPlaying] = useState(false);
   const videoRef = useRef(null);
 
   useEffect(() => {
@@ -51,7 +51,7 @@ const VideoPage = () => {
           return prevIndex;
         }
       });
-    }, getDurationForNextIndex(currentIndex, transcript) * 1000); // Duration in milliseconds
+    }, getDurationForNextIndex(currentIndex, transcript) * 500); // Duration in milliseconds
 
     return () => clearInterval(interval);
   }, [transcript, currentIndex, isPlaying]);
