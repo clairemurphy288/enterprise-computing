9e2d9c29cf24ae221fa381a6da39f3af9d264479
Claire Murphy <murphyclaire91@gmail.com>
2024-07-21 19:54:41 -0400

    Revert "Revert "congratulations page""
    This reverts commit 2f33dfb8f06b259079109c41ea188c47c58ecef1.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import SignUpPage from './SignUpPage';
 import PasswordResetPage from './PasswordResetPage';
 import HomePage from './HomePage';
 import VideoPage from './VideoPage';
-import FlashcardDecksPage from './FlashcardDecks';
+import FlashcardDecks from './FlashcardDecks'; // Correct path if needed
 import PracticePage from './PracticePage';
+import CompletionPage from './CompletionPage'; // Import the new page component
 import PrivateRoute from './PrivateRoute';
 
 function App() {
@@ -18,14 +19,16 @@ function App() {
         <Route path="/reset-password" element={<PasswordResetPage />} />
         <Route path="/home" element={<PrivateRoute element={<HomePage />} />} />
         <Route path="/video/:id" element={<PrivateRoute element={<VideoPage />} />} />
-        <Route path="/flashcard-decks" element={<PrivateRoute element={<FlashcardDecksPage />} />} />
+        <Route path="/flashcard-decks" element={<PrivateRoute element={<FlashcardDecks />} />} />
         <Route path="/practice/:deckId" element={<PrivateRoute element={<PracticePage />} />} />
+        <Route path="/completion/:deckId" element={<PrivateRoute element={<CompletionPage />} />} /> {/* Add route for Completion Page */}
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
 
 
 
